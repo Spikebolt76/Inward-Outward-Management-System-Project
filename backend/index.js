7148c152c9b35e75210dcc6b2bba7910abe8ea5c
Spikebolt76 <jinesh.s745@gmail.com>
2026-01-20 08:28:32 +0530

    idk bro

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+const app = express()
+const PORT = 3000
+
+app.use(json())
+app.get('/', (req, res) => {
+    res.send("Welcome to the Inward-Outward Management Interface!");
+});
+
+app.post('/', (req, res) => {
+    
+    res.send(req.body);
+})
+
+app.listen(PORT, () => {
+    console.log("Server listening on port : ", PORT);
+});
+
