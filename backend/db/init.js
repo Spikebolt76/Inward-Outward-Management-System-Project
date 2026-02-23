10a3b11d63a0606d058784c96af825173ae09cc8
Spikebolt76 <jinesh.s745@gmail.com>
2026-02-23 16:34:04 +0530

    deleted the commented code 🤦🏻‍♂️

--- backend/db/init.js
+++ backend/db/init.js
@@ -2,20 +2,6 @@ const sequelize = require("./connection");
 const {User, Institute, Department, FinancialYear, Inward, Outward,
     InOutwardFromTo, InOutwardMode, InOutwardOffice, CourierCompany} = require("../models/associations")
 
-// async function initDB() {  ------> Database has been created, no need to run this code
-//     try{
-//         await sequelize.authenticate();
-//         console.log("Connected to MySQL server");
-
-//         await sequelize.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`)
-//         console.log("Database created or already exists");
-//     } catch(err) {
-//         console.log("Error: ", err);
-//     } finally {
-//         await sequelize.close();
-//     }
-// }   
-
 async function startConnection() {
     try { 
         await sequelize.authenticate();
